2cfc86e156c8e246a6bda8e3113aeaaba949d6f1
Anurag pandey <149105080+anuragyes@users.noreply.github.com>
2026-02-10 20:25:51 +0530

    Change API base URL to production endpoint

--- frontend/src/Redux/Feature/Api.js
+++ frontend/src/Redux/Feature/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://annotation-project-real-time-collab.onrender.com/api",
 });
 
 // Attach token automatically
